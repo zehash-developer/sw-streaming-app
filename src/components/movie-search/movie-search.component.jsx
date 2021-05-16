97771e4250c51cde21e874077c9525efa8435f61
Hashim <hashim.raza@mediamonks.com>
2021-05-16 13:05:27 +0500

    Added MovieSearch component JSX and SCSS files.

---
+++ src/components/movie-search/movie-search.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import TextField from '@material-ui/core/TextField';
+
+const MovieSearch = () => {
+  return (
+    <div className="search">
+      <TextField id="streaming-search" label="Search" />
+    </div>
+  );
+};
+
+export default MovieSearch;
